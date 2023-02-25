ede35f3d2bcc6c6682525129ac6a3b031561ce3a
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-02-25 10:38:11 +0500

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.result = 0;
     }
 
     guess() {
-
+        let res = Math.round((this.max + this.min) / 2);
+        this.result = res; 
+        return res;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
+
 
 module.exports = GuessingGame;
